32916f058d615c9bae3663c1e4e01c96399b1329
interdictor <interdictor.dsg@gmail.com>
2019-06-06 12:00:54 +0200

    Use environment variables for configuration

--- src/config_provider.py
+++ src/config_provider.py
@@ -2,11 +2,15 @@ import os
 
 
 class ConfigProvider:
+    DEFAULT_HOST = '0.0.0.0'
+    DEFAULT_WORKERS = 4
+    DEFAULT_WSGI_TIMEOUT = 10
+
     @classmethod
     def provide(cls):
         return {
-            'port': 8000,
-            'host': '0.0.0.0',
-            'workers': 4,
-            'wsgi_timeout': 30,
+            'port': os.getenv('PORT'),
+            'host': os.getenv('HOST', cls.DEFAULT_HOST),
+            'workers': os.getenv('WORKERS', cls.DEFAULT_WORKERS),
+            'wsgi_timeout': os.getenv('WSGI_TIMEOUT', cls.DEFAULT_WSGI_TIMEOUT),
         }
