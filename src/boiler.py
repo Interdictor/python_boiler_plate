6499d8067019acf1c4cae6a5f48c767b0549ad5b
interdictor <interdictor.dsg@gmail.com>
2019-06-07 11:15:01 +0200

    Add templates

--- src/boiler.py
+++ src/boiler.py
@@ -1,8 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+app = Flask(
+    __name__,
+    static_folder='delivery/static',
+    template_folder='delivery/templates',
+)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('hello.html')
 
 app.config['PROPAGATE_EXCEPTIONS'] = False
